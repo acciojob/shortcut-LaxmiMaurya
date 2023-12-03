9dbe4fd136670529ee49ba346aa38d84677fad61
Laxmi <91832050+LaxmiMaurya@users.noreply.github.com>
2023-12-03 19:40:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function shortcut(s1, s2) {
   // your code here
+	function shortcut(str1, str2) {
+    // Check if either input string is empty
+    if (str1.length === 0 || str2.length === 0) {
+        return '';
+    }
+
+    // Get the initial letters of the input strings
+    const initialLetters = str1.charAt(0).toUpperCase() + str2.charAt(0).toLowerCase();
+
+    return initialLetters;
+}
 }
 
 // Do not change the code below.
